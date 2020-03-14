8c86bae47dbd0578b560a443db1936cd8d8deadc
Vishnu Srivastava <vishnu-srivastava@hotmail.com>
2020-03-14 10:54:32 -0400

    test

---
+++ test.py
@@ -0,0 +1,8 @@
+from git import Repo
+import os
+import sys
+
+repo = Repo(os.path.abspath('.'))
+notebook_path = sys.argv[0].split('/')[-1]
+repo.index.add([notebook_path])
+repo.index.commit(message='test')
